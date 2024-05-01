3a624da21e757ace27adbb255e6b99233da71c91
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-05-01 16:11:19 -0700

    Home component created

---
+++ react-portfolio/src/components/Home.js
@@ -0,0 +1,15 @@
+// Import React
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className='app-body'>
+           <h1>Home</h1>
+
+            <div className='banner'></div> 
+            <div className='banner'></div> 
+            <div className='banner'></div> 
+            <div className='banner'></div> 
+        </div>  
+    )
+}
