b5625670c7f617fa7a8c3e7190770dba2194d63a
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-05-04 20:17:30 -0700

    .container and .box and .paragraph

--- react-portfolio/src/components/About.js
+++ react-portfolio/src/components/About.js
@@ -10,29 +10,42 @@ export default function About() {
             
             <h1 className='center'>About</h1>
 
-            <p>Don is a web developer, writer, and artist.</p>
+            <p className='paragraph'>
+                Don is a web developer, writer, and artist based in Riverside, California. They have used all of these deciplines to design 
+                eLearning Tutorials as well as web sites for University and private clients. Don is currently looking for employment in web 
+                development.
+            </p>
 
             <h2>Experience</h2>
 
-            <h3>University of California, Riverside, Library - eLearning Developer</h3>
-            <p>2021 - 2023</p>
+            <div className='container'>
+                <div className='box'>
+                    <h3>University of California, Riverside, Library - eLearning Developer</h3>
+                    <p>2021 - 2023</p>
 
-            <ul>
-                <li>Develop Qualtrics Netpromoter surveys that access the public response to our tutorials</li>
-                <li>Utilize and create tutorials on database search methodologies and related topics</li>
-                <li>Design elearning tutorials and wep pages for 10,000+ students</li>
-                <li>Develop new skills to support these tasks such as video editing, programming, and data collection</li>
-            </ul>
+                    <ul>
+                        <li>Develop Qualtrics Netpromoter surveys that access the public response to our tutorials</li>
+                        <li>Utilize and create tutorials on database search methodologies and related topics</li>
+                        <li>Design elearning tutorials and wep pages for 10,000+ students</li>
+                        <li>Develop new skills to support these tasks such as video editing, programming, and data collection</li>
+                    </ul>
+                </div>
 
-            <h3>Gamespawn - Outreach Coordinator</h3>
-            <p>2021  2023</p>
+                <div className='box'>
+                    <h3>Gamespawn - Outreach Coordinator</h3>
+                    <p>2021  2023</p>
 
-            <ul>
-                <li>Schedule and host speaking events for about 20 university students</li>
-                <li>Email and call potential speakers for our organization via networking</li>
-                <li>Design posters advertising upcoming speaking events</li>
-                <li>Maintain speaking event schedules in GSuite and record attendance</li>
-            </ul>
+                    <ul>
+                        <li>Schedule and host speaking events for about 20 university students</li>
+                        <li>Email and call potential speakers for our organization via networking</li>
+                        <li>Design posters advertising upcoming speaking events</li>
+                        <li>Maintain speaking event schedules in GSuite and record attendance</li>
+                    </ul>
+                </div>
+            </div>
+            
+
+            
 
             <h2>Education</h2>
             
