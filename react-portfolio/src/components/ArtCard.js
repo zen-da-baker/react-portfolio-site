ed6bf62c4668b478d2c277f35117f3e42e02cac8
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-05-05 13:14:57 -0700

    Remove border on cards

--- react-portfolio/src/components/ArtCard.js
+++ react-portfolio/src/components/ArtCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function ArtCard(props) {
     return (
-        <div className='Card'>
+        <div className='card'>
             <div className='card-img-container'>
                 <img className='card-img' src={props.imgSrc} alt={props.alt} />
             </div>
