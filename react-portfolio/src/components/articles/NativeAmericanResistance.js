c5d9f2e62186100f6d925864b890d8cc2369d5cf
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-05-18 11:48:03 -0700

    NativeAmericanResistance component created

---
+++ react-portfolio/src/components/articles/NativeAmericanResistance.js
@@ -0,0 +1,78 @@
+// Import React
+import React from 'react';
+
+export default function NativeAmericanResistance() {
+    return (
+        <div className="content">
+        
+        <h1 className="center-div">Caught Between Power Blocs</h1>
+
+        <div className="center-div">
+            <figcaption><a href="../index.html">Donovan Frazier</a> | Mar 5, 2022 | Updated Feb 25, 2023</figcaption>
+        </div>
+
+        <div className="center-div">
+
+            <p>In Eastern North America, there were two major power blocs forming in the 1700s. Along the eastern coast, there were the colonists of 
+                various European empires such as the British, Dutch, and Spanish. To the west of them was the Iroquois, six tribal nations that were a 
+                united front against the expansion of the European colonists. In the middle were several of the tribal nations we have discussed in the 
+                class so far. These peoples in the middle often had more difficulty mounting successful resistance to the colonists because of three 
+                main obstacles which were political divisions, size, and the number of different colonists they were opposing, but there were a couple 
+                factors that helped with resistance.</p>
+
+            <p>The first obstacle for these middle indigenous people was the political divisions within the tribal nations. Unlike the Yaquis or Pueblo 
+                peoples who had united their individual tribal nations in the far west, the middle indigenous people to the east at times faced much heavier 
+                difficulties uniting. One such example was the seven Cherokee clans in which resistance against the British was hampered by blame for failures 
+                being shifted around the various clans. This demonstrated that even though the Cherokee were seen as a single people, the Cherokee themselves 
+                didn’t necessarily act in unity. This could also be observed with the Chickamauga Cherokee splitting off into their own separate faction in the 
+                years following the Seven Years’ War with many of their people migrating out because of disagreements with the change in leadership. Splits such 
+                as these would cause the tribal nation to shrink, making forming a unified resistance against the colonists even more difficult than it already was. </p>
+
+            <p>A second obstacle for these middle indigenous people was the size of their tribal nations. There were examples of tribal nations being too large to 
+                coordinate effective resistance as well as being too small to do so. One such example was that the Cherokee peoples were so large or spread out enough 
+                that Cherokee people in different regions spoke different languages. This would make relaying information for coordinating a unified resistance difficult. 
+                On the other end of the spectrum, tribal nations such as the Mohegan and Pequot in New England were outnumbered by the colonists at the time of King Philip’s War. 
+                Being outnumbered outside of being the cause of fears of the colonist to begin with, was also a militaristic disadvantage that made armed resistance difficult. </p>
+
+            <p>A third obstacle was the different colonists in the area forming a single bloc of power as seen with the various New England colonies. These colonies 
+                were originally separate but shortly before King Philip’s War, formed a unified power bloc at the Plymouth War Council. The united colonists in 
+                the northeast formed an effective offensive against the indigenous tribal nations caught between the two blocs. Not only this, but there were also 
+                the Spanish to the southwest and the French to the northwest. If this information was placed on a map, it would appear that the European 
+                colonists were encircling the middle indigenous people which would have been quite alarming if word traveled across the many different languages. 
+                The unity of the colonists as a single power bloc in places such as New England meant that divisions within tribal nations put the tribal nations 
+                at a severe disadvantage since the tribal nations would have internal struggles to deal with on top of the external conflicts like disease and 
+                colonial expansion. </p>
+
+            <p>In spite of these obstacles, there were also factors that helped the indigenous people resist the colonists. One factor was that non-combatants served 
+                an immensely important role in holding the society together. An example of this was seen in praying towns being used as refuge in the aftermath of 
+                King Philip’s War. These towns helped survivors collect together in a safe place which held the society in place instead of complete collapse. The tribal 
+                nations were still persisting after offensives by the colonists. Another example was the maintenance of culture under women such as Cockacoeske that held 
+                the spiritual and cultural fabric of the society together in spite of massive losses from war and the deaths of leaders. The fabric being held together 
+                was a sign that the society was still resisting even in the aftermath of a defeat during war.</p>
+
+            <p>One more factor that helped the middle indigenous people was intratribal cooperation. This was seen with the divided New England tribal nations coming 
+                together for a unified response against the expanding British as seen with economic unity in blocking land sales to the colonists. This form of 
+                resistance was effective, but the Iroquois would be the most effective at using their united power bloc to broker peace on behalf of surrounding 
+                indigenous peoples. They used their politics to successfully prevent war with the expanding British before the Seven Years’ War and maintain their 
+                resistance to colonialism. </p>
+
+            <p>As seen, there were a variety of factors that hindered resistance movements of indigenous peoples caught between the main two power blocs in 
+                Eastern North America as well as some factors that helped resistance movements. Middle peoples such as the Cherokee, Mohegan, and Pequot were the 
+                least unified and struggled the most to mount resistance against colonialism because of it. But other indigenous power blocs that were more centralized 
+                such as the Iroquois or faced smaller colonial forces such as the Yaquis in the west faired much better. The people caught between the power blocs 
+                suffered the most from direct conflict and weaker resistance to the colonists. </p>
+
+        </div>
+
+        <div className="center-div">
+            <button>View Comments (This is just a test feature, not actual comments)</button>
+        </div>
+
+        <div className="center-div">
+            <p>If you wish to return to the orignal portfolio page, feel free to use the links in the footer to return to that section of the website</p>
+        </div>
+
+
+    </div>
+    )
+}
