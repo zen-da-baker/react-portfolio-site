e9f46566b6e715f54880655048f1ae929b44443c
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-07-01 08:06:23 -0700

    Programming project order and math added

--- react-portfolio/src/components/Programming.js
+++ react-portfolio/src/components/Programming.js
@@ -28,35 +28,19 @@ export default function Programming() {
 
             <div className='gallery'>
                 <Card 
-                    title={data.programming.program1.title} 
-                    description={data.programming.program1.description}
-                    href={data.programming.program1.href}
-                    src={data.programming.program1.src}
-                    alt={data.programming.program1.alt}
+                    title={data.programming.program7.title} 
+                    description={data.programming.program7.description}
+                    href={data.programming.program7.href}
+                    src={data.programming.program7.src}
+                    alt={data.programming.program7.alt}
                 />
 
                 <Card 
-                    title={data.programming.program2.title}
-                    description={data.programming.program2.description}
-                    href={data.programming.program2.href}
-                    src={data.programming.program2.src}
-                    alt={data.programming.program2.alt}
-                />
-
-                <Card 
-                    title={data.programming.program3.title}
-                    description={data.programming.program3.description}
-                    href={data.programming.program3.href}
-                    src={data.programming.program3.src}
-                    alt={data.programming.program3.alt}
-                />
-
-                <Card 
-                    title={data.programming.program4.title}
-                    description={data.programming.program4.description}
-                    href={data.programming.program4.href}
-                    src={data.programming.program4.src}
-                    alt={data.programming.program4.alt}
+                    title={data.programming.program6.title}
+                    description={data.programming.program6.description}
+                    href={data.programming.program6.href}
+                    src={data.programming.program6.src}
+                    alt={data.programming.program6.alt}
                 />
 
                 <Card 
@@ -68,10 +52,34 @@ export default function Programming() {
                 />
 
                 <Card 
-                    title={data.programming.program6.title}
-                    description={data.programming.program6.description}
-                    src={data.programming.program6.src}
-                    alt={data.programming.program6.alt}
+                    title={data.programming.program4.title}
+                    description={data.programming.program4.description}
+                    href={data.programming.program4.href}
+                    src={data.programming.program4.src}
+                    alt={data.programming.program4.alt}
+                />
+
+                <Card 
+                    title={data.programming.program3.title}
+                    description={data.programming.program3.description}
+                    href={data.programming.program3.href}
+                    src={data.programming.program3.src}
+                    alt={data.programming.program3.alt}
+                />
+
+                <Card 
+                    title={data.programming.program2.title}
+                    description={data.programming.program2.description}
+                    href={data.programming.program2.href}
+                    src={data.programming.program2.src}
+                    alt={data.programming.program2.alt}
+                />
+
+                <Card 
+                    title={data.programming.program1.title}
+                    description={data.programming.program1.description}
+                    src={data.programming.program1.src}
+                    alt={data.programming.program1.alt}
                 />
             </div>
         </div>
